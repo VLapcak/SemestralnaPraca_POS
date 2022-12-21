01ad1c09d8973797fa1824b85fef4657e533d810
VLapcak <100422772+VLapcak@users.noreply.github.com>
2022-12-21 13:35:49 +0100

    SDL2 integration

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
-#include <SDL.h>
+#include <SDL2/SDL.h>
 
-int main(int argc, char* args[]) {
-    std::cout << "Hello, World!" << std::endl;
+
+int main(int argc, char* argv[]) {
+
+    SDL_Window* window;
+
+    window = SDL_CreateWindow("Clovece nehnevaj sa", 0, 0, 800, 600, SDL_WINDOWEVENT_SHOWN);
+
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+
     return 0;
+
 }
